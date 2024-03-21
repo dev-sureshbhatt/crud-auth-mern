5a4d65d25cb72af2bd736eb76d17d8fc14b2fdbf
dev-sureshbhatt <dev.sureshbhatt@gmail.com>
2024-03-21 15:31:14 +0530

    Updated question form (minor changes)

--- client/src/pages/CreateForm.jsx
+++ client/src/pages/CreateForm.jsx
@@ -1,25 +1,19 @@
 import React, { useState } from "react";
 
 export default function CreateForm() {
-
-   const [formContent, setFormContent] = useState([]) 
-
+  const [formContent, setFormContent] = useState([]);
 
   function addQuestion() {
     const fields = {
-        "number": formContent.length,
-        "name": `question_${formContent.length}`,
-        "label": "untitled_question",
-        "type": "short_answer",
-        "list": []
-    }
-    setFormContent([...formContent, fields])
+      number: formContent.length,
+      name: `question_${formContent.length}`,
+      label: "untitled_question",
+      type: "short_answer",
+      list: [],
+    };
+    setFormContent([...formContent, fields]);
   }
-  
-  
-  
-  
-  
+
   return (
     <div className="p-3 mx-auto flex flex-col">
       <h1 className="text-3xl text-center font-semibold my-7">
@@ -27,29 +21,36 @@ export default function CreateForm() {
       </h1>
 
       <div className="bg-white rounded-lg shadow-md flex flex-col justify-center p-5">
-        
         <div>
-            {
-                formContent.map((item, index)=>{
-                    return(
-                        <div>
-                        <div key={item.name}>
-                        <p>{item.number+1}</p>
-                        <p>{item.label}</p>
-                        </div>
-                        <div>
-
-                        </div>
-                        </div>
-                    )
-                })
-            }
+          {formContent.map((item, index) => {
+            return (
+                <div>
+              <div className="flex justify-between space-y-6 items-center">
+                <div className="flex gap-1" key={item.name}>
+                  <p>{item.number + 1}</p>
+                  <p>{item.label}</p>
+                </div>
+                <div>
+                  <select>
+                    <option value="short_answer">Short Answer</option>
+                    <option value="paragraph">Paragraph</option>
+                    <option value="multichoice">Multichoice</option>
+                  </select>
+                </div>
+              
+              </div>
+              <div>
+                {item.type == "short_answer" && <input type="text" placeholder={item.label} />}
+                {item.type == "paragraph" && <textarea placeholder={item.label} />}
+                {item.type == "multichoice" && <input type="text" placeholder={item.label} />}
+              </div>
+              
+              </div>);
+          })}
         </div>
 
-        <div>
-             
-        </div>
         
+
         <button
           onClick={addQuestion}
           className="bg-black text-white p-3 mx-auto rounded-md"
