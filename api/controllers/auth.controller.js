87ef3f0ce566579f1c9f234715b848d567da42ac
dev-sureshbhatt <dev.sureshbhatt@gmail.com>
2024-03-21 07:59:00 +0530

    Bcrypt password hashing setup done

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,24 @@
+import bcryptjs from 'bcryptjs'
+
 export const signup = (req, res) => {
     const {email, password} = req.body
     console.log(req.body)
-    res.json({"msg": "working"})
+
+    try {
+
+        const hashedPassword = bcryptjs.hashSync(password, 8)
+        console.log(hashedPassword)
+
+        const isValid = bcryptjs.compareSync('1111', hashedPassword)
+        console.log(isValid)
+
+
+
+        res.json({"msg": "working"})    
+    } catch (error) {
+        
+    }
+
+    
 
 }
